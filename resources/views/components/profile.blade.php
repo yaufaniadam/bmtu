ce2324e5933ef84e14da1d8a3c4e8bdd0dbe5395
yaufani adam <yaufaniadam@yahoo.com>
2023-02-15 12:03:21 +0700

    'navbar'

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -2,5 +2,9 @@
     aria-expanded="false">
     <span class="mr-2 d-none d-lg-inline text-gray-600 small"> Profil Saya</span>
     <img class="img-profile rounded-circle"
-        src="{{ url('image?file='.$employee_photo) ?? asset('img/undraw_profile.svg') }}">
+
+    @php
+        $photo = ($employee_photo) ? 'image?file=' . $employee_photo : asset('img/noprofileimg.jpg');
+    @endphp
+        src="{{ url( $photo )  }}">
 </a>
