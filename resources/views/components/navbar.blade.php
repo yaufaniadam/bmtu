bc63b3b8b740530c6e9204546e1854ce6361a4c7
DESKTOP-EB8EN4I\X240 <waskitodamar17@gmail.com>
2023-03-30 09:51:10 +0700

    profile pic fix

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -29,8 +29,10 @@
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ $employee_name }}</span>
-                <img class="img-profile rounded-circle"
-                    src="{{ url('image?file='.isset($employee_photo)) ?? asset('img/undraw_profile.svg') }}">
+                @php
+                    $photo = ($employee_photo) ? 'image?file=' . $employee_photo : asset('img/noprofileimg.jpg');
+                @endphp
+                <img class="img-profile rounded-circle" src="{{ url( $photo ) }}">
             </a>
 
             <!-- Dropdown - User Information -->
